757aa3840b207eff6c2e8f921932d08e57e504d4
egr-meister <dulingleb@gmail.com>
2026-08-04 02:06:53 +0300

    Fix Retrofit kotlinx-serialization converter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,7 +153,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    // Official Square Kotlinx Serialization converter (same version as Retrofit).
+    // Provides retrofit2.converter.kotlinx.serialization.asConverterFactory.
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     debugImplementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
